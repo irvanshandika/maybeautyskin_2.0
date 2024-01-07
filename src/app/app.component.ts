4a5bf239ca4d708df0e091afd395334dc2bc1fa4
irvanshandika <shandikamuhammadirvan@gmail>
2024-01-07 18:14:27 +0700

    Add error pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { initFlowbite } from 'flowbite';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { initFlowbite } from "flowbite";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent implements OnInit {
-  title = 'web-app';
-
   ngOnInit(): void {
     initFlowbite();
   }
 }
-
--- src/app/errorpages/errorpages.component.ts
+++ src/app/errorpages/errorpages.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-errorpages',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './errorpages.component.html',
   styleUrl: './errorpages.component.css'
 })
