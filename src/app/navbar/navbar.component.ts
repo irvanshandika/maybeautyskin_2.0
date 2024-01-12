66e893f3692b602a3ebefd37b3b3f1eddfe881f9
irvanshandika <shandikamuhammadirvan@gmail>
2024-01-13 00:51:31 +0700

    Trying fix SEO And Make a Display of The Services Provided at This Store

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -27,4 +27,21 @@ export class NavbarComponent {
       url: "pages/perawatan",
     },
   ];
+  layananKami = [
+    {
+      images: "https://res.cloudinary.com/dszhlpm81/image/upload/v1705079441/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/customer_service_yy3jfj.png",
+      alt: "Customer Service",
+      description: "Customer Service 24 Jam Siap Melayani Anda",
+    },
+    {
+      images: "https://res.cloudinary.com/dszhlpm81/image/upload/v1705080985/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/shipping_lmfkdd.png",
+      alt: "Shipping",
+      description: "Pengiriman Cepat dan Aman",
+    },
+    {
+      images: "https://res.cloudinary.com/dszhlpm81/image/upload/v1705079380/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/delivery_chffqb.png",
+      alt: "Keselamatan Produk",
+      description: "Paket Selamat Sampai Ke Tangan Konsumen",
+    },
+  ];
 }
